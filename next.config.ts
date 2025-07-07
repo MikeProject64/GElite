4a1f489977eafaa4374a2bff7f6378765632b4cf
Riffusion <riffusion8@gmail.com>
2025-07-07 12:23:06 +0000

    E isso aqui? Da pra melhorar a performace?

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -24,6 +25,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2040, 3840],
   },
 };
 
