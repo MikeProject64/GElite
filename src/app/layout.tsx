ef030b59c39132616957cab544646f4f95da7f10
Riffusion <riffusion8@gmail.com>
2025-07-07 12:24:32 +0000

    Retire o favicon do firebase do projeto, por favor. Ele as vezes aparece

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
     template: '%s | Gestor Elite',
   },
   description: 'Otimize sua gestão de serviços com o Gestor Elite.',
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>‍</text></svg>",
+  },
 };
 
 export default function RootLayout({
