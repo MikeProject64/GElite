a450669f7e293c61316ba24df0876ef13530eaac
Riffusion <riffusion8@gmail.com>
2025-07-08 14:39:24 +0000

    Instalar o Gerenciador de tags do Google 1. Cole este código o mais alto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N73C2JGT');`}}></script>
+        {/* End Google Tag Manager */}
+
         {/* Google Ads Tag */}
         <script
           async
@@ -77,6 +85,10 @@ fbq('track', 'PageView');
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
       )}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N73C2JGT"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>
           {children}
         </Providers>
