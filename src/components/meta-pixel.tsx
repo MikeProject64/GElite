a996954092c8b4ad567999651916c7bedb099811
Desenvolvedor <dev.projects.225@gmail.com>
2025-07-09 20:35:31 +0000

    <!-- Meta Pixel Code --> <script> !function(f,b,e,v,n,t,s) {if(f.fbq)ret

--- src/components/meta-pixel.tsx
+++ src/components/meta-pixel.tsx
@@ -18,11 +18,23 @@ const MetaPixel = () => {
   return (
     <>
       <Script
-        id="fb-pixel"
-        src="/scripts/pixel.js"
+        id="fb-pixel-base"
         strategy="afterInteractive"
         onLoad={() => setLoaded(true)}
-        data-pixel-id={pixel.PIXEL_ID}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixel.PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
       />
     </>
   )
--- src/lib/meta-pixel.ts
+++ src/lib/meta-pixel.ts
@@ -1,10 +1,7 @@
 
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
-
-export const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID
+export const PIXEL_ID = '3232172553603431'
 
 export const pageview = () => {
   if (typeof window.fbq !== 'undefined') {
