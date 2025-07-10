926f9bef3b37fe0580553073c6cb2bbc7c06eb98
Riffusion <riffusion8@gmail.com>
2025-07-10 22:42:25 +0000

    Por favor, reveja as configurações no codigo pra ver se as tags Google e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GT-P3NGQJ5T');`}}></script>
+        })(window,document,'script','dataLayer','GTM-N73C2JGT');`}}></script>
         {/* End Google Tag Manager */}
 
         {/* Google Ads Tag */}
@@ -65,7 +65,7 @@ export default function RootLayout({
         fontSans.variable
       )}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GT-P3NGQJ5T"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N73C2JGT"
         height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         {/* End Google Tag Manager (noscript) */}
         <Providers>
