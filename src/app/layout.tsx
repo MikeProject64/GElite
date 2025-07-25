16a7f1eaad56c272908afbade5b81d88a399b313
Riffusion <riffusion8@gmail.com>
2025-07-25 00:30:28 +0000

    Altere somente o favicon na aba do navegador , eu tava pensando nesse:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,16 +55,16 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-// Corrigido para ser uma função simples que retorna JSX estático
 async function getDynamicHeadElements() {
-  const color = { h: 210, s: 70, l: 40 }; // Cor azul padrão do foguete
-  
+  const color = { h: 221.2, s: 83.2, l: 53.3 }; // Azul Padrão
+
   // SVG estático do foguete azul, conforme solicitado.
   const rocketSvgString = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="none">
-        <path fill="hsl(${color.h} ${color.s}% ${color.l}%)" d="M117.21 230.91a10.29 10.29 0 0 1-10.2-12.83l33.6-100.83a10.29 10.29 0 1 1 19.55 6.51l-33.6 100.83a10.29 10.29 0 0 1-9.35 6.32Z"/>
-        <path fill="hsl(${color.h} ${color.s}% ${color.l}%)" d="M152.89 123a43.16 43.16 0 1 0-86.32 0 43.16 43.16 0 0 0 86.32 0Z"/>
-        <path fill="hsl(${color.h} ${color.s}% ${color.l}%)" d="M168.16 230.91a10.29 10.29 0 0 1-10.2-12.83l33.6-100.83a10.29 10.29 0 1 1 19.55 6.51l-33.6 100.83a10.29 10.29 0 0 1-9.35 6.32Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="hsl(${color.h}, ${color.s}%, ${color.l}%)" stroke="none">
+      <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"></path>
+      <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"></path>
+      <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"></path>
+      <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"></path>
     </svg>
   `.trim();
   
@@ -72,7 +72,7 @@ async function getDynamicHeadElements() {
 
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: `:root { --primary: ${color.h} ${color.s}% ${color.l}%; }` }} />
+      <style dangerouslySetInnerHTML={{ __html: `:root { --primary-h: ${color.h}; --primary-s: ${color.s}%; --primary-l: ${color.l}%; }` }} />
       <link id="dynamic-favicon" rel="icon" href={faviconUrl} sizes="any" />
     </>
   );
